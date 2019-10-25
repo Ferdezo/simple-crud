df1ad32d34eefb350109f01501749cfec2ce000c
Arkadiusz Migała <arkadiusz.migala@performgroup.com>
2019-10-25 09:46:06 +0200

    Java and Spring Boot upgrade for JDK 13

--- src/main/java/com/arkadiusz/migala/simplecrud/SimpleCrudApplication.java
+++ src/main/java/com/arkadiusz/migala/simplecrud/SimpleCrudApplication.java
@@ -7,9 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 @EnableWebMvc
 public class SimpleCrudApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimpleCrudApplication.class, args);
     }
-
 }
